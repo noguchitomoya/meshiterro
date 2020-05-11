2376aa345145ab84f9d01fba178b6d34c9a73b03
野口智矢 <noguchitomoya@noguchisatoshiyanoMacBook-Pro.local>
2020-05-11 17:52:23 +0900

    [Finish] 6-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '9f9f5755df8b44595b27f7c4c56552f2439e3e9373f38c8cf87c1557a1962c753e597c2dedb19bf6153cb7545afc3a8c139f3e2a72a81fdd6843be335b5c3a19'
